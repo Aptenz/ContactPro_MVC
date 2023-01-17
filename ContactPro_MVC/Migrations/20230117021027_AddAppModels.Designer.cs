376642cf6e6f7ded73d184aebf8ece1a4a4633cd
Aptenz <59879368+Aptenz@users.noreply.github.com>
2023-01-16 19:14:13 -0700

    Added models for categories and contacts

--- ContactPro_MVC/Migrations/20230117021027_AddAppModels.Designer.cs
+++ ContactPro_MVC/Migrations/20230117021027_AddAppModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ContactPro_MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ContactPro_MVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230117021027_AddAppModels")]
+    partial class AddAppModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
