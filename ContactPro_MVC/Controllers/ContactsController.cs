54feaa9070774c057684fa1bd30e22994bddbe9c
Aptenz <59879368+Aptenz@users.noreply.github.com>
2023-01-26 19:19:28 -0700

    Allowed for filtering contacts by categories and name

--- ContactPro_MVC/Controllers/ContactsController.cs
+++ ContactPro_MVC/Controllers/ContactsController.cs
@@ -31,11 +31,66 @@ namespace ContactPro_MVC.Controllers
 
         // GET: Contacts
         [Authorize]
-        public async Task<IActionResult> Index()
+        public IActionResult Index(int categoryId)
         {
-            var applicationDbContext = _context.Contacts.Include(c => c.AppUser);
-            return View(await applicationDbContext.ToListAsync());
+
+            List<Contact> contacts = new List<Contact>();
+            string appUserId = _userManager.GetUserId(User);
+
+            //return userID and its associated contacts and associated categories;
+            AppUser appUser = _context.Users.Include(c => c.Contacts)
+                                            .ThenInclude(c => c.Categories)
+                                            .FirstOrDefault(u => u.Id == appUserId)!;
+
+            var categories = appUser.Categories;
+
+            if (categoryId == 0)
+            {
+                contacts = appUser.Contacts.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+            } else
+            {
+                contacts = appUser.Categories.FirstOrDefault(c => c.Id == categoryId)!
+                                  .Contacts
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList();
+            }
+
+            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name");
+
+            return View(contacts);
         }
+
+        [Authorize]
+        public IActionResult SearchContacts(string searchString)
+        {
+            string appUserId = _userManager.GetUserId(User);
+            var contacts = new List<Contact>();
+
+            AppUser appUser = _context.Users
+                                      .Include(c => c.Contacts)
+                                      .ThenInclude(c => c.Categories)
+                                      .FirstOrDefault(u => u.Id == appUserId)!;
+
+            if(String.IsNullOrEmpty(searchString))
+            {
+                contacts = appUser.Contacts
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList();
+            } else
+            {
+                contacts = appUser.Contacts.Where(c => c.FullName!.ToLower().Contains(searchString.ToLower()))
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList();
+            }
+
+            ViewData["CategoryId"] = new SelectList(appUser.Categories, "Id", "Name",0);
+
+            return View(nameof(Index), contacts);
+        }
+
 
         // GET: Contacts/Details/5
         [Authorize]
--- ContactPro_MVC/Models/AppUser.cs
+++ ContactPro_MVC/Models/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema; // markup our class here to effect the sql generation process
 using Microsoft.AspNetCore.Identity;
 
@@ -12,11 +13,14 @@ namespace ContactPro_MVC.Models
         public string? FirstName { get; set; }
 
         [Required]
-        [Display(Name = "First Name")]
+        [Display(Name = "Last Name")]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1} characters long.", MinimumLength = 2)]
         public string? LastName { get; set; }
 
         [NotMapped] // Won't be in the database, calculated at runtime
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
     }
 }
