2d0dbf4748098f0823946db95b0f257489d24759
Aptenz <59879368+Aptenz@users.noreply.github.com>
2023-02-03 14:29:07 -0700

    Changed post action for edit category

--- ContactPro_MVC/Controllers/CategoriesController.cs
+++ ContactPro_MVC/Controllers/CategoriesController.cs
@@ -123,6 +123,8 @@ namespace ContactPro_MVC.Controllers
             {
                 try
                 {
+                    string appUserId = _userManager.GetUserId(User);
+                    category.AppUserId = appUserId;
                     _context.Update(category);
                     await _context.SaveChangesAsync();
                 }
@@ -139,7 +141,6 @@ namespace ContactPro_MVC.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
             return View(category);
         }
 
